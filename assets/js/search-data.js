d27d3717654f12c6003adb9711065a30242a1dd7
p20250093-afk <p20250093@hyderabad.bits-pilani.ac.in>
2026-01-06 03:50:32 +0000

    Deploying to gh-pages from @ p20250093-afk/nn@74efbf9443433b68765d4d1495dd100011c4e7d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,13 +455,6 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/nn/projects/9_project/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/nn/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
