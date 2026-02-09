70943d99b5c8c05f2a279a9005807804d6244866
p20250093-afk <p20250093@hyderabad.bits-pilani.ac.in>
2026-02-09 04:36:22 +0000

    Deploying to gh-pages from @ nits1114/LAB@9d838fd1f696e39e054fe3d8ca5ea453f54f7beb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/LAB/publications/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/LAB/projects/";
+          },
         },{id: "nav-cv",
           title: "CV",
           description: "",
